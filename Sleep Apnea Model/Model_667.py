275b9ac362d2fe88cacd69c8bc3957b49fa203f7
YukktaSeelam <84347464+YukktaSeelam@users.noreply.github.com>
2022-11-16 23:50:12 +0530

    Update Model_667.py

--- Sleep Apnea Model/Model_667.py
+++ Sleep Apnea Model/Model_667.py
@@ -7,7 +7,8 @@
 
 # In[3]:
 
-
+#importing necessary libraries.
+#pip install wfdb
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
 import numpy as np
@@ -20,7 +21,7 @@ from sklearn.utils import class_weight
 
 # In[4]:
 
-
+#to check if code is running or GPU
 tf.test.is_built_with_cuda()
 
 from tensorflow.python.client import device_lib
@@ -31,7 +32,7 @@ device_lib.list_local_devices()
 
 # In[5]:
 
-
+# To store weights of epoch with best accuracy.
 save_here = os.path.join("/home/era/yukkta", "apn.h5")
 cp_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=save_here,
@@ -42,20 +43,17 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(
 
 # In[6]:
 
-
+# Loading and reshaping data to fit the model 
 sequence_length = 240
 
-
-# In[7]:
-
-
-X_train = np.load('/home/era/yukkta/train_input.npy', allow_pickle=True)
+X_train = np.load('/home/era/yukkta/train_input.npy', allow_pickle=True)  #change path as necessary.
 y_train = np.load('/home/era/yukkta/train_label.npy', allow_pickle=True)
 
 X_test = np.load('/home/era/yukkta/test_input.npy', allow_pickle=True)
 y_test = np.load('/home/era/yukkta/test_label.npy', allow_pickle=True)
 
-
+# The waveform related data is stored in X1
+# Sex and age related data is stored in X2.
 X1 = []
 X2 = []
 for index in range(len(X_train)):
@@ -72,16 +70,19 @@ for index in range(len(X_test)):
     X2.append([X_test[index][2], X_test[index][3]])
 X_test1, X_test2 = np.array(X1).astype('float64'), np.array(X2).astype('float64')
 
+#data is reshaped to fit the model input dimensions
 X_train1 = np.transpose(X_train1, (0, 2, 1))
 
 X_test1 = np.transpose(X_test1, (0, 2, 1))
 
+'''since most patients do not have sleep apnea, it is ensential to ensure that the model is not 
+biased towards the more probable prediction. Therefore class weights are used.'''
 class_w = class_weight.compute_class_weight(class_weight = "balanced", classes = np.unique(y_train), y = y_train)
 
 
 # In[7]:
 
-
+#MODEL ARCHETECTURE
 layers = {'input': 2, 'hidden1': 256, 'hidden2': 256, 'output': 1}
 x1 = tf.keras.layers.Input(shape=(sequence_length, layers['input']))
 m1 = tf.keras.layers.LSTM(layers['hidden1'],                   
@@ -105,6 +106,7 @@ out = tf.keras.layers.Activation("sigmoid")(out)
 
 model = tf.keras.models.Model(inputs=[x1, x2], outputs=[out])
 
+#loss function and optimizer
 model.compile(loss="binary_crossentropy", optimizer="adam",
               metrics = ['accuracy'])
 
@@ -112,9 +114,9 @@ model.compile(loss="binary_crossentropy", optimizer="adam",
 model.summary()
 
 
-# In[ ]:
+# In[8]:
 
-
+#Training starts
 class_w = {i : class_w[i] for i in range(2)}
 history = model.fit([X_train1, X_train2], y_train, epochs=20, batch_size=256, validation_split=0.1, class_weight=class_w, callbacks=[cp_callback])
 
